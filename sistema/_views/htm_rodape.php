f09c185d1f936fd03951e06e226d0b7c47b3a8b2
rgdweb <contatorgdweb@gmail.com>
2026-04-26 11:37:53 -0300

    limpando

--- sistema/_views/htm_rodape.php
+++
@@ -1,4 +0,0 @@
-<?php if(!isset($_base['libera_views'])){ header("HTTP/1.0 404 Not Found"); exit; } ?>
-<footer class="main-footer"> 
-	<div class="pull-right hidden-xs"></div>
-	<strong>Copyright &copy; <?=date('Y')?> </strong> todos os direitos reservados - <a target="_blank">POR Márcio Gomes</a></footer>
